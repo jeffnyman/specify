84bf1b2197ad675810bdf305374b1bd70144efd2
Jeff Nyman <jeffnyman@gmail.com>
2018-12-11 09:23:54 -0600

    Addition of step blocks.

--- lib/specify.rb
+++ lib/specify.rb
@@ -8,6 +8,8 @@ require "specify/rspec/example_group"
 
 module Specify
   ::RSpec::Core::ExampleGroup.define_example_method :Scenario, with_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :Condition, with_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :Behavior, with_steps: true
 
   ::RSpec::Core::ExampleGroup.send :include, RSpec::Specify::ExampleGroup
   ::RSpec::Core::Reporter.send :include, RSpec::Specify::Reporter
--- spec/specify/step_block_spec.rb
+++ spec/specify/step_block_spec.rb
@@ -1,4 +1,19 @@
 RSpec.describe "top-level describe block" do
   Scenario "step block (Scenario)" do
   end
+
+  Condition "step block (Condition)" do
+  end
+
+  Behavior "step block (Behavior)" do
+  end
+end
+
+Feature "feature" do
+  Scenario "scenario" do
+    expect {
+      Condition "condition" do
+      end
+    }.to raise_error RSpec::Core::ExampleGroup::WrongScopeError
+  end
 end
