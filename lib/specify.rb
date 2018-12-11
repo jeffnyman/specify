8326fdfcfc6714539b2e36eb29ea6825e63e2696
Jeff Nyman <jeffnyman@gmail.com>
2018-12-11 12:50:16 -0600

    Addition of step blocks.

--- lib/specify.rb
+++ lib/specify.rb
@@ -11,6 +11,20 @@ module Specify
   ::RSpec::Core::ExampleGroup.define_example_method :Condition, has_steps: true
   ::RSpec::Core::ExampleGroup.define_example_method :Behavior, has_steps: true
 
+  ::RSpec::Core::ExampleGroup.define_example_method :scenario, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :condition, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :behavior, has_steps: true
+
+  ::RSpec::Core::ExampleGroup.define_example_method :Steps, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :Tests, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :Rules, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :Facts, has_steps: true
+
+  ::RSpec::Core::ExampleGroup.define_example_method :steps, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :rules, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :tests, has_steps: true
+  ::RSpec::Core::ExampleGroup.define_example_method :facts, has_steps: true
+
   ::RSpec::Core::ExampleGroup.send :include, RSpec::Specify::ExampleGroup
   ::RSpec::Core::Reporter.send :include, RSpec::Specify::Reporter
 end
--- spec/specify/step_block_spec.rb
+++ spec/specify/step_block_spec.rb
@@ -2,10 +2,43 @@ RSpec.describe "top-level describe block" do
   Scenario "step block (Scenario)" do
   end
 
+  scenario "step block (scenario)" do
+  end
+
   Condition "step block (Condition)" do
   end
 
+  condition "step block (condition)" do
+  end
+
   Behavior "step block (Behavior)" do
+  end
+
+  behavior "step block (behavior)" do
+  end
+
+  Steps "step block (Step)" do
+  end
+
+  steps "step block (Step)" do
+  end
+
+  Tests "step block (Test)" do
+  end
+
+  tests "test block (test)" do
+  end
+
+  Rules "step block (Rule)" do
+  end
+
+  rules "rule block (rule)" do
+  end
+
+  Facts "step block (Fact)" do
+  end
+
+  facts "step block (fact)" do
   end
 end
 
